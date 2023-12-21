95dbc3235cbfb3b71da6126ce2a4f07ee69f8711
phippstech <37256286+phippstech@users.noreply.github.com>
2023-12-21 16:19:38 -0600

    first commit

---
+++ src/hal_implementation/hal_esp8266_i2c.cpp
@@ -0,0 +1,99 @@
+#include <string.h>
+#include <stdio.h>
+#include <Arduino.h>
+#include <Wire.h>
+extern "C" {
+#include <cryptoauthlib.h>
+}
+#define SDA_PIN 4 // Define the GPIO pin for SDA
+#define SCL_PIN 5 // Define the GPIO pin for SCL
+
+#define MAX_I2C_BUSES 1 // ESP8266 has 1 I2C bus
+
+typedef struct atcaI2Cmaster
+{
+    int id;
+    int ref_ct;
+} ATCAI2CMaster_t;
+
+ATCAI2CMaster_t i2c_hal_data[MAX_I2C_BUSES];
+
+ATCA_STATUS hal_i2c_change_baud(ATCAIface iface, uint32_t speed)
+{
+    Wire.setClock(speed);
+    return ATCA_SUCCESS;
+}
+
+ATCA_STATUS hal_i2c_init(ATCAIface iface, ATCAIfaceCfg *cfg)
+{
+    int bus = 0; // ESP8266 has only one I2C bus
+    i2c_hal_data[bus].id = bus;
+    i2c_hal_data[bus].ref_ct = 1;
+
+    Wire.begin(SDA_PIN, SCL_PIN);
+
+    iface->hal_data = &i2c_hal_data[bus];
+
+    return ATCA_SUCCESS;
+}
+
+ATCA_STATUS hal_i2c_post_init(ATCAIface iface)
+{
+    // Not required on ESP8266
+    return ATCA_SUCCESS;
+}
+
+ATCA_STATUS hal_i2c_send(ATCAIface iface, uint8_t word_address, uint8_t *txdata, int txlength)
+{
+    ATCAIfaceCfg *cfg = atgetifacecfg(iface);
+    String payload = "";
+    for(int i=0; i < txlength; i++) {
+        if(txdata[i] <= 0x0f) 
+            payload += "0";
+        payload += String(txdata[i], HEX);
+    }
+    int address = cfg->atcai2c.slave_address;
+    Serial.println(
+        "I2C send " + 
+        String(txlength) + 
+        " bytes to addr 0x" + 
+        String(address, HEX) + ": " + payload);
+    Wire.beginTransmission(address);
+    Wire.write(txdata, txlength);
+    Wire.endTransmission();
+    return ATCA_SUCCESS;
+}
+
+ATCA_STATUS hal_i2c_receive(ATCAIface iface, uint8_t address, uint8_t *rxdata, uint16_t *rxlength)
+{
+    ATCAIfaceCfg *cfg = atgetifacecfg(iface);
+    int wantRead = *rxlength;
+    Wire.requestFrom((int)cfg->atcai2c.slave_address, wantRead);
+    int i = 0;
+    while (Wire.available() && i < *rxlength)
+    {
+        rxdata[i++] = Wire.read();
+    }
+
+    *rxlength = i;
+    Serial.println(
+        "I2C received " + 
+        String((int)*rxlength) + 
+        " bytes from addr 0x" + 
+        String(cfg->atcai2c.slave_address, HEX));
+    if (*rxlength == 0)
+        return ATCA_COMM_FAIL;
+    return ATCA_SUCCESS;   
+}
+
+ATCA_STATUS hal_i2c_release(void *hal_data)
+{
+    // Not required on ESP8266
+    return ATCA_SUCCESS;
+}
+
+ATCA_STATUS hal_i2c_control(ATCAIface iface, uint8_t option, void *param, size_t paramlen)
+{
+    // Not supported on ESP8266
+    return ATCA_UNIMPLEMENTED;
+}
---
+++ src/main.cpp
@@ -0,0 +1,195 @@
+#include <Arduino.h>
+extern "C" {
+#include "cryptoauthlib.h"
+}
+#include "Configuration.h"
+
+uint8_t KEY_SLOT = (uint8_t)9;
+
+ATCAIfaceCfg cfg;
+ATCA_STATUS status;
+
+// Example of AES KEY (len 32)
+uint8_t example_of_key[32] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+
+// Exemple of configuration for ATECC608A. Look at page 8 on the datasheet for ATECC608A-TFLXTLS
+static uint8_t configuration_example[112] = 
+{
+  //The first 16 bytes can't be modified
+    0x60, // I2C address<br> *Byte 16*      
+    0x00, //Reserved, must be zero *Byte 17*
+    0x00, //CountMatch *Byte 18*
+    0x00, //ChipMode,0x00 means the device in in high speed *byte19*
+///////////////////////////////////////////////////////////////////////////////////////////////
+    //*bytes 20 through 51* The data zone
+    0x83, 0x20, //  Slot Config Slot 1                     ***This where I think the issue is what this code
+    0x85, 0x20, //  Slot Config Slot 2                     After the "lock configuration" prompt goes away, 
+    0x8F, 0x20, //  Slot Config Slot 3                      the chip is no longer usable*****
+    0xC4, 0x8F, //  Slot Config Slot 4
+    0x8F, 0x8F, //  Slot Config Slot 5
+    0x8F, 0x8F, //  Slot Config Slot 6
+    0x9F, 0x8F, //  Slot Config Slot 7
+    0x0F, 0x0F, //  Slot Config Slot 8
+    0x8F, 0x0F, //  Slot Config Slot 9
+    0x8F, 0x0F, //  Slot Config Slot 10
+    0x8F, 0x0F, //  Slot Config Slot 11
+    0x8F, 0x0F, //  Slot Config Slot 12
+    0x8F, 0x0F, //  Slot Config Slot 13
+    0x00, 0x00, //  Slot Config Slot 14
+    0x00, 0x00, //  Slot Config Slot 15
+    0xAF, 0x8F, //  Slot Config Slot 16
+    ////////////////////////////////////////////////////////////////////
+    //*Bytes 52 though 59
+    0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00,
+    //*Bytes 60 through 67
+    0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00,
+    0x00,//UseLock *Byte 68*
+    0x00,//VolatileKeyPermission *Byte 69*
+    0x00, 0x00,//SecureBoot *Byte 70 and 71*
+    0x00, //KdflvLoc *Byte 72*
+    0x00, 0x00, //KdflvStr *Byte 73 and 74*
+    // reserved, must be zero *Bytes 75 through 83*
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00,//UserExtra *Byte 84*
+    0x00,//UserExtraAdd *Byte 85*
+    0x00,//LockValue *Byte 86*
+    0x00,//LockConfig *Byte 87*
+    0xFF, 0xFF,//SlotLocked *Byte 88 and 89*
+    0x00, 0x00, //ChipOptions *Byte 90 and 91*
+    0x00, 0x00, 0x00, 0x00,//X509format *Byte 92 through 95
+
+    //KeyConfig *Byte 96 through 127*
+    0x33, 0x00, //  Key Config Slot 1
+    0x33, 0x00, //  Key Config Slot 2
+    0x33, 0x00, //  Key Config Slot 3
+    0x1C, 0x00, //  Key Config Slot 4
+    0x1C, 0x00, //  Key Config Slot 5
+    0x1C, 0x00, //  Key Config Slot 6
+    0x1C, 0x00, //  Key Config Slot 7
+    0x3C, 0x00, //  Key Config Slot 8
+    0x1A, 0x00, //  Key Config Slot 9
+    0x3A, 0x00, //  Key Config Slot 10
+    0x1A, 0x00, //  Key Config Slot 11
+    0x3A, 0x00, //  Key Config Slot 12
+    0x3A, 0x00, //  Key Config Slot 13
+    0x3C, 0x00, //  Key Config Slot 14
+    0x3C, 0x00, //  Key Config Slot 15
+    0x1C, 0x00  //  Key Config Slot 16
+};
+
+bool menu(String message)
+{
+  Serial.println(message);
+  while (!Serial.available())
+  {
+    ;
+  }
+  String answer = Serial.readString();
+  return (answer == "yes") ? true : menu(message);
+}
+
+void setup()
+{
+  Serial.begin(74880);
+
+  // Init the constuctor for the library
+  cfg.iface_type = ATCA_I2C_IFACE;  // Type of communication -> I2C mode
+  cfg.devtype = ATECC608A;          // Type of chip
+  cfg.atcai2c.slave_address = 0X60; // I2C address of Adafruit device
+  cfg.atcai2c.bus = 1;
+  cfg.atcai2c.baud = 100000;
+  cfg.wake_delay = 1500; // Delay of wake up (1500 ms)
+  cfg.rx_retries = 20;
+  Serial.println("\nFirmware start!");
+  // by default logs to stderr, which is not visible printed by printf() in Arduino.
+  // redirect to stdout instead to makeit  visible.
+  atca_trace_config(stdout);
+}
+
+void loop()
+{
+
+  status = atcab_init(&cfg);
+  if (status == ATCA_SUCCESS)
+  {
+    //Write the configuration
+    if (!menu(F("Do you want to write the configuration ?")))
+      return;
+    status = write_configuration(&cfg, configuration_example, sizeof(configuration_example));
+    if (status == ATCA_SUCCESS)
+    {
+      // Lock the configuration Zone
+      if (!menu(F("Do you want to lock the Configuration zone (No more change can be done after that) ?")))
+        return;
+      status = lock_zone(&cfg, LOCK_ZONE_CONFIG);
+      if (status == ATCA_SUCCESS)
+      {
+        // Write the key in the given slot
+        if (!menu(F("Do you want to write the key in the given slot ?")))
+          return;
+        status = write_key_slot(&cfg, KEY_SLOT, example_of_key, sizeof(example_of_key));
+        if (status == ATCA_SUCCESS)
+        {
+          // Lock the Data Zone
+          if (!menu(F("Do you want to lock the Data zone (No more change can be done after that) ?")))
+            return;
+          status = lock_zone(&cfg, LOCK_ZONE_DATA);
+          if (status == ATCA_SUCCESS)
+          {
+            ATCA_STATUS lock_config = check_lock_zone(&cfg, LOCK_ZONE_CONFIG);
+            ATCA_STATUS lock_data = check_lock_zone(&cfg, LOCK_ZONE_DATA);
+            if (lock_config == ATCA_SUCCESS && lock_data == ATCA_SUCCESS)
+            {
+              Serial.println("Your chip has been set, you can use it now !");
+              return;
+            }
+            else
+            {
+              if (lock_config != ATCA_SUCCESS)
+                Serial.println("Your config zone has not been locked !");
+              if (lock_data != ATCA_SUCCESS)
+                Serial.println("Your data zone has not been locked !");
+              return;
+            }
+          }
+          else
+          {
+            // See file atca_status.h for the code Error
+            Serial.print(F("Impossible to Lock data | Code Error 0x"));
+            Serial.println(status, HEX);
+            return;
+          }
+        }
+        else
+        {
+          // See file atca_status.h for the code Error
+          Serial.print(F("Impossible to Write the key | Code Error 0x"));
+          Serial.println(status, HEX);
+          return;
+        }
+      }
+      else
+      {
+        // See file atca_status.h for the code Error
+        Serial.print(F("Impossible to Lock configuration | Code Error 0x"));
+        Serial.println(status, HEX);
+        return;
+      }
+    }
+    else
+    {
+      // See file atca_status.h for the code Error
+      Serial.print(F("Impossible to Write configuration | Code Error 0x"));
+      Serial.println(status, HEX);
+      return;
+    }
+  }
+  else
+  {
+    // See file atca_status.h for the code Error
+    Serial.print(F("Impossible to Init configuration | Code Error 0x"));
+    Serial.println(status, HEX);
+    return;
+  }
+}
+
